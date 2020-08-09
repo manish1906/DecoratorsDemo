ee3dc303365da76fd8f2a12b7a4db8fcd9bb9138
manish106 <parmarmanishj2012@gmail.com>
2020-08-09 19:16:05 +0530

    Add decorators demo

---
+++ Decorators/module.js
@@ -0,0 +1,23 @@
+﻿angular.module('app', [])   
+    .service('myService', function () {
+        console.log("service");
+        this.getDate = function () {
+            console.log("service fn");
+            return null; 
+        };
+    })
+    .controller('myController', function (myService) {
+        console.log("controller");
+        var vm = this;
+        vm.date = myService.getDate();
+    })
+    .config(function ($provide) {
+        console.log("config");
+        $provide.decorator('myService', function ($delegate) {
+            console.log("decorator");
+            $delegate.getDate = function () {
+                return new Date();
+            };
+            return $delegate;
+        });
+    });
